880c8d056a5ab9435afc0fde5c5ab7f601135aae
APPLE <appleblade22@gmail.com>
2022-06-26 11:48:58 +0530

    Done
    Done

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '1e094aa466mshd74e02dc878c35ap154d80jsn70e7363add55',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
